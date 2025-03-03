02f3b74aab5f30e60a197edcbb68152cea8a88d4
azza20435 <zuuzaali12@gmail.com>
2025-03-03 14:06:58 +0300

    change modal_form invoice

--- app/Views/invoices/modal_form.php
+++ app/Views/invoices/modal_form.php
@@ -53,24 +53,24 @@
                 </div>
             </div>
 
-            <?php if (count($companies_dropdown) > 1) { ?>
+            <!-- </?php if (count($companies_dropdown) > 1) { ?>
                 <div class="form-group">
                     <div class="row">
-                        <label for="company_id" class=" col-md-3"><?php echo app_lang('company'); ?></label>
+                        <label for="company_id" class=" col-md-3"></?php echo app_lang('company'); ?></label>
                         <div class="col-md-9">
-                            <?php
-                            echo form_input(array(
-                                "id" => "company_id",
-                                "name" => "company_id",
-                                "value" => $model_info->company_id,
-                                "class" => "form-control",
-                                "placeholder" => app_lang('company')
-                            ));
+                            </?php
+                            // echo form_input(array(
+                            //     "id" => "company_id",
+                            //     "name" => "company_id",
+                            //     "value" => $model_info->company_id,
+                            //     "class" => "form-control",
+                            //     "placeholder" => app_lang('company')
+                            // ));
                             ?>
                         </div>
                     </div>
                 </div>
-            <?php } ?>
+            </?php } ?> -->
 
             <?php if ($client_id && !$project_id) { ?>
                 <input type="hidden" name="invoice_client_id" value="<?php echo $client_id; ?>" />
