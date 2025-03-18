5503f8b3a793e95f32fda2614610e46e41ee35a4
azza20435 <zuuzaali12@gmail.com>
2025-03-18 16:02:00 +0300

    form

--- plugins/aleelo_plugin/Views/invoices/modal_form.php
+++ plugins/aleelo_plugin/Views/invoices/modal_form.php
@@ -270,12 +270,12 @@
             <?php echo view("includes/dropzone_preview"); ?>
         </div>
     </div>
+    </div>
 
     <div class="modal-footer">
         <?php echo view("includes/upload_button"); ?>
         <button type="button" class="btn btn-default" data-bs-dismiss="modal"><span data-feather="x" class="icon-16"></span> <?php echo app_lang('close'); ?></button>
         <button type="submit" class="btn btn-primary"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
-    </div>
 </div>
 <?php echo form_close(); ?>
 
