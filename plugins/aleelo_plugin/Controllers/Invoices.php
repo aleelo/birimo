f644ce9e37ea91ab7cccf55727b751a9d81a6b71
azza20435 <zuuzaali12@gmail.com>
2025-04-10 15:38:10 +0300

    asd

--- plugins/aleelo_plugin/Controllers/Invoices.php
+++ plugins/aleelo_plugin/Controllers/Invoices.php
@@ -150,7 +150,8 @@ class Invoices extends Security_Controller_Plugin {
         $view_data['clients_dropdown'] = array("" => "-") + $this->Clients_model->get_dropdown_list(array("company_name"), "id", array("is_lead" => 0,"company_id"=>$department));
 
     }
-    else if($this->login_user->is_admin == 1 &&! $this->login_user->company_access= "all" ){
+    
+    else if ($this->login_user->is_admin == 1 && $this->login_user->company_access != "all") {        
         $view_data['clients_dropdown'] = array("" => "-") + $this->Clients_model->get_dropdown_list(array("company_name"), "id", array("is_lead" => 0));
 
     }
