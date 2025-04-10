bdfe362586eb9226e20b2c61b84ee980f35f195e
azza20435 <zuuzaali12@gmail.com>
2025-04-10 15:00:18 +0300

    100

--- plugins/aleelo_plugin/Views/invoices/invoice_pdf.php
+++ plugins/aleelo_plugin/Views/invoices/invoice_pdf.php
@@ -195,8 +195,8 @@
         <?php echo $company_info->Condition_company; ?>
     <?php } ?>
    </td>
-    <td style="width: 15%;"></td>
-    <td style="width: 35%; vertical-align: top; text-align: left; padding: 0px;">
+    <td style="width: 10%;"></td>
+    <td style="width: 40%; vertical-align: top; text-align: left; padding: 0px;">
   
   
   
