e0b0e6a1c450080e127a8637ca1230bd6f536446
azza20435 <zuuzaali12@gmail.com>
2025-04-10 14:37:12 +0300

    0

--- plugins/aleelo_plugin/Views/invoices/invoice_pdf.php
+++ plugins/aleelo_plugin/Views/invoices/invoice_pdf.php
@@ -196,7 +196,7 @@
     <?php } ?>
    </td>
     <td style="width: 15%;line-height: -0;"></td>
-    <td style="width: 35%; line-height: 1; vertical-align: top; text-align: left; padding: 0px;">
+    <td style="width: 35%; line-height: 0; vertical-align: top; text-align: left; padding: 0px;">
   
   
   
