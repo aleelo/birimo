1c72bb425661d6fd04b7321d9f83e4fc2e8ecd03
azza20435 <zuuzaali12@gmail.com>
2025-04-12 14:15:47 +0300

    Quotation

--- plugins/aleelo_plugin/Views/estimates/estimate_parts/estimate_to.php
+++ plugins/aleelo_plugin/Views/estimates/estimate_parts/estimate_to.php
@@ -4,7 +4,7 @@
 <?php if (get_setting("invoice_style") != "style_3") { ?>
 <?php } ?>
 <?php if ($estimate_info->estimate_date) { ?>
-        <br /> <strong style="font-size: 1.1em; color: <?php echo $color; ?>;"><?php echo ("ESTIMATE#"); ?>:</strong> <?php echo $estimate_info->id; }?>
+        <br /> <strong style="font-size: 1.1em; color: <?php echo $color; ?>;"><?php echo ("Quotation#"); ?>:</strong> <?php echo $estimate_info->id; }?>
 
         <span class="invoice-meta text-default">
     <?php if ($estimate_info->estimate_date) { ?>
