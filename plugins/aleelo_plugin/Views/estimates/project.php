fd581cd9e23dfa2f6c0ab65b11eade096c52592f
azza20435 <zuuzaali12@gmail.com>
2025-04-14 14:24:40 +0300

    saave footer

--- plugins/aleelo_plugin/Views/estimates/project.php
+++ plugins/aleelo_plugin/Views/estimates/project.php
@@ -27,7 +27,7 @@
 
     </div>
 </div>
-
+<div class="modal-footer">
     <button type="button" class="btn btn-default" data-bs-dismiss="modal"><span data-feather="x" class="icon-16"></span> <?php echo app_lang('close'); ?></button>
     <button type="submit" class="btn btn-primary"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
 </div>
