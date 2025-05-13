daa61f15ad35757fdfb2c9939f77a16aa6227ec5
azza20435 <zuuzaali12@gmail.com>
2025-05-13 14:10:06 +0300

    back

--- app/Libraries/Pdf.php
+++ app/Libraries/Pdf.php
@@ -20,6 +20,16 @@ class Pdf extends \TCPDF {
         $this->invoice_data = $data;
     }
     public function Header() {
+        $break_margin = $this->getBreakMargin();
+        $auto_page_break = $this->AutoPageBreak;
+        $this->SetAutoPageBreak(false, 0);
+
+        $img_file = get_file_from_setting("invoice_pdf_background_image", false, get_setting("timeline_file_path"));
+        $this->Image($img_file, 0, 0, 210, 297, '', '', '', false, 500, '', false, false, 0);
+
+        // restore auto-page-break status
+        $this->SetAutoPageBreak($auto_page_break, $break_margin);
+        
         if ($this->pdf_type == 'invoice') {
             $client_info = isset($this->invoice_data['client_info']) ? $this->invoice_data['client_info'] : null;
 
