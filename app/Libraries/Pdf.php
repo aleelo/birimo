af4f4d8bf48d3c5f30d3b0acd36e108163c660e6
azza20435 <zuuzaali12@gmail.com>
2025-05-13 14:09:18 +0300

    pusg

--- app/Libraries/Pdf.php
+++ app/Libraries/Pdf.php
@@ -54,18 +54,9 @@ class Pdf extends \TCPDF {
             <br><br><br> 
 ';
             
-            // $this->writeHTMLCell(0, 0, '', '', $data, 0, 1, 0, true, '', true);
+            $this->writeHTMLCell(0, 0, '', '', $data, 0, 1, 0, true, '', true);
         
-            $break_margin = $this->getBreakMargin();
-            $auto_page_break = $this->AutoPageBreak;
-            $this->SetAutoPageBreak(false, 0);
 
-            $img_file = get_file_from_setting("invoice_pdf_background_image", false, get_setting("timeline_file_path"));
-            $this->Image($img_file, 0, 0, 210, 297, '', '', '', false, 500, '', false, false, 0);
-
-            // restore auto-page-break status
-            $this->SetAutoPageBreak($auto_page_break, $break_margin);
-            
         } else {
             // call the original Header method from the parent class
             parent::Header();
@@ -112,26 +103,7 @@ class Pdf extends \TCPDF {
             $this->SetY(-5); 
             
             $this->writeHTMLCell(0, 0, '', '', $data, 0, 1, 0, true, '', true);
-            $data2 = '
-            <div style="background-color: '.$color.';
-                        color: white;
-                        font-size: 24px;
-                        font-weight: bold;
-                        width: 100%;
-                        padding: 1px 2px;
-                        line-height: 51;
-                        height: 2px;
-                        
-            padding: 100px;">
-            </div>
             
-            <br><br><br> 
-            ';
-    
-            $this->SetY(0); 
-            $this->setX(-1100);
-            
-            $this->writeHTMLCell(0, 0, '', '', $data2, 0, 1, 0, true, '', true);
         } else {
             parent::Footer();
         }
