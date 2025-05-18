aec81dfe5bf8ee93be59b1a473651bb03ddde25c
azza20435 <zuuzaali12@gmail.com>
2025-05-18 15:14:30 +0300

    delete due

--- plugins/aleelo_plugin/Views/invoices/delivery_note_parts/bill_to.php
+++ plugins/aleelo_plugin/Views/invoices/delivery_note_parts/bill_to.php
@@ -11,14 +11,14 @@
                 <?php echo format_to_date($invoice_info->bill_date); ?>
             </td>
         </tr>
-        <tr>
+        <!-- <tr>
             <td style="font-size: 0.8em; color:black; font-weight: bold;text-align: right;">
                 <?php echo (" Due Date:"); ?>
             </td>
             <td>
                 <?php echo format_to_date($invoice_info->due_date); ?>
             </td>
-        </tr>
+        </tr> -->
        
     </table>
 <?php } ?>
