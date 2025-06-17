661baa8bcc5470544b6b358d46f4c3ff5f1cef81
azza20435 <zuuzaali12@gmail.com>
2025-06-17 15:28:38 +0300

    Update Security_Controller.php update function permision hidelist hr

--- app/Controllers/Security_Controller.php
+++ app/Controllers/Security_Controller.php
@@ -114,17 +114,12 @@ class Security_Controller extends App_Controller {
     }
 
     //only allowed to access for team members 
-protected function access_only_team_members() {
-    if (!$this->login_user->is_admin) {
-        $hide_team_members = get_array_value($this->login_user->permissions, "hide_team_members_list") == "1";
-        $show_staff = get_array_value($this->login_user->permissions, "show_staff") == "1";
-
-        if ($hide_team_members || !$show_staff) {
+    protected function access_only_team_members() {
+        $task_permission = get_array_value($this->login_user->permissions, "hide_team_members_list") =="1";
+        if (!$this->login_user->is_admin && get_array_value($this->login_user->permissions, "hide_team_members_list") == "1" ) {
             app_redirect("forbidden");
         }
     }
-}
-
 
     //only allowed to access for admin users
     protected function access_only_admin() {
