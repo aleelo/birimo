5dac44e31c390ebf36d2535891d239aa369f3b01
fahadmumin <fahadmumin8@gmail.com>
2025-07-05 12:50:48 -0700

    dfd

--- plugins/aleelo_plugin/Helpers/demo_general_helper.php
+++ plugins/aleelo_plugin/Helpers/demo_general_helper.php
@@ -11,7 +11,8 @@ use App\Libraries\Pdf; // Adjust the namespace based on your project structure
 
 if (!function_exists('get_demo_setting')) {
 
-    function get_demo_setting($key = "") {
+    function get_demo_setting($key = "")
+    {
         $config = new aleelo_plugin\Config\Demo();
 
         $setting_value = get_array_value($config->app_settings_array, $key);
@@ -21,11 +22,69 @@ if (!function_exists('get_demo_setting')) {
             return "";
         }
     }
-
 }
+
+if (!function_exists('update_custom_fields_changes')) {
+
+    function update_custom_fields_changes($related_to_type, $related_to_id, $changes, $activity_log_id = 0)
+    {
+        // if ($changes && count($changes)) {
+        if (is_array($changes) && count($changes)) {
+            $ci = new App_Controller();
+
+            $related_to_data = new \stdClass();
+
+            $log_type = "";
+            $log_for = "";
+            $log_type_title = "";
+            $log_for_id = "";
+
+            if ($related_to_type == "tasks") {
+                $related_to_data = $ci->Tasks_model->get_one($related_to_id);
+                $log_type = "task";
+                $log_for = "project";
+                $log_type_title = $related_to_data->title;
+                $log_for_id = $related_to_data->project_id;
+            }
+
+            $log_data = array(
+                "action" => "updated",
+                "log_type" => $log_type,
+                "log_type_title" => $log_type_title,
+                "log_type_id" => $related_to_id,
+                "log_for" => $log_for,
+                "log_for_id" => $log_for_id
+            );
+
+            if ($activity_log_id) {
+                $before_changes = array();
+
+                //we have to combine with the existing changes of activity logs
+                $activity_log = $ci->Activity_logs_model->get_one($activity_log_id);
+                $activity_logs_changes = unserialize($activity_log->changes ? $activity_log->changes : "");
+                if (is_array($activity_logs_changes)) {
+                    foreach ($activity_logs_changes as $key => $value) {
+                        $before_changes[$key] = array("from" => get_array_value($value, "from"), "to" => get_array_value($value, "to"));
+                    }
+                }
+
+                $log_data["changes"] = serialize(array_merge($before_changes, $changes));
+
+                if ($activity_log->action != "created") {
+                    $ci->Activity_logs_model->update_where($log_data, array("id" => $activity_log_id));
+                }
+            } else {
+                $log_data["changes"] = serialize($changes);
+                return $ci->Activity_logs_model->ci_save($log_data);
+            }
+        }
+    }
+}
+
 if (!function_exists('get_team_member_profile_link')) {
 
-    function get_team_member_profile_link($id = 0, $name = "", $attributes = array()) {
+    function get_team_member_profile_link($id = 0, $name = "", $attributes = array())
+    {
         $ci = new Security_Controller_Plugin(false);
         if ($ci->login_user->user_type === "staff") {
             return anchor("team_member/view/" . $id, $name ? $name : "", $attributes);
@@ -42,18 +101,19 @@ if (!function_exists('get_team_member_profile_link')) {
  */
 if (!function_exists('demo_load_css')) {
 
-    function demo_load_css(array $array) {
+    function demo_load_css(array $array)
+    {
         $version = get_setting("app_version");
 
         foreach ($array as $uri) {
             echo "<link rel='stylesheet' type='text/css' href='" . base_url(PLUGIN_URL_PATH . "Demo/$uri") . "?v=$version' />";
         }
     }
-
 }
 if (!function_exists('prepare_estimate_pdf')) {
 
-    function prepare_estimate_pdf($estimate_data, $mode = "download") {
+    function prepare_estimate_pdf($estimate_data, $mode = "download")
+    {
         $pdf = new Pdf("invoice");
         if (!get_setting("enable_background_image_for_invoice_pdf")) {
             $pdf->setPrintHeader(false);
@@ -61,7 +121,7 @@ if (!function_exists('prepare_estimate_pdf')) {
         $pdf->setPrintFooter(false);
         $pdf->SetCellPadding(1.5);
         $pdf->setImageScale(1.42);
-                $pdf->setInvoiceData($estimate_data); // 
+        $pdf->setInvoiceData($estimate_data); // 
 
         $pdf->AddPage();
 
@@ -95,7 +155,8 @@ if (!function_exists('prepare_estimate_pdf')) {
 }
 if (!function_exists('prepare_estimate_pdff')) {
 
-    function prepare_estimate_pdff($estimate_data, $mode = "download") {
+    function prepare_estimate_pdff($estimate_data, $mode = "download")
+    {
         $pdf = new Pdf("invoice");
         if (!get_setting("enable_background_image_for_invoice_pdf")) {
             $pdf->setPrintHeader(false);
@@ -103,7 +164,7 @@ if (!function_exists('prepare_estimate_pdff')) {
         $pdf->setPrintFooter(false);
         $pdf->SetCellPadding(1.5);
         $pdf->setImageScale(1.42);
-                $pdf->setInvoiceData($estimate_data); // 
+        $pdf->setInvoiceData($estimate_data); // 
 
         $pdf->AddPage();
 
@@ -137,7 +198,8 @@ if (!function_exists('prepare_estimate_pdff')) {
 }
 if (!function_exists('get_estimate_making_data')) {
 
-    function get_estimate_making_data($estimate_id) {
+    function get_estimate_making_data($estimate_id)
+    {
         validate_numeric_value($estimate_id);
         $ci = new Security_Controller_Plugin();
 
@@ -147,14 +209,14 @@ if (!function_exists('get_estimate_making_data')) {
             $data['client_info'] = $ci->Clients_model->get_one($data['estimate_info']->client_id);
             $data['company_info'] = $ci->Company_model->get_one($data['client_info']->company_id);
             $data['users_info'] = $ci->Users_models->get_one($data['company_info']->finance_manager_id);
-          
-            $finance_manager_info = $ci->db->table('team_member_job_info')
-            ->select('*') // Select user_id and job_title_en
-            ->where('user_id', $data['company_info']->finance_manager_id)
-            ->get()
-            ->getRow();
 
-        $data['finance_manager_info'] = $finance_manager_info;
+            $finance_manager_info = $ci->db->table('team_member_job_info')
+                ->select('*') // Select user_id and job_title_en
+                ->where('user_id', $data['company_info']->finance_manager_id)
+                ->get()
+                ->getRow();
+
+            $data['finance_manager_info'] = $finance_manager_info;
 
             $data['estimate_items'] = $ci->Estimate_items_model->get_details(array("estimate_id" => $estimate_id))->getResult();
             $data["estimate_total_summary"] = $ci->Estimates_model->get_estimate_total_summary($estimate_id);
@@ -175,7 +237,8 @@ if (!function_exists('get_estimate_making_data')) {
  */
 if (!function_exists('get_invoice_making_data')) {
 
-    function get_invoice_making_data($invoice_id) {
+    function get_invoice_making_data($invoice_id)
+    {
         $ci = new Security_Controller_Plugin();
         $invoice_info = $ci->Invoices_model->get_details(array("id" => $invoice_id))->getRow();
         if ($invoice_info) {
@@ -187,12 +250,12 @@ if (!function_exists('get_invoice_making_data')) {
             $data['company_info'] = $ci->Company_model->get_one($data['client_info']->company_id);
             $data['users_info'] = $ci->Users_models->get_one($data['company_info']->finance_manager_id);
             $finance_manager_info = $ci->db->table('team_member_job_info')
-            ->select('*') // Select user_id and job_title_en
-            ->where('user_id', $data['company_info']->finance_manager_id)
-            ->get()
-            ->getRow();
+                ->select('*') // Select user_id and job_title_en
+                ->where('user_id', $data['company_info']->finance_manager_id)
+                ->get()
+                ->getRow();
 
-        $data['finance_manager_info'] = $finance_manager_info;
+            $data['finance_manager_info'] = $finance_manager_info;
             $data['invoice_info']->custom_fields = $ci->Custom_field_values_model->get_details(array("related_to_type" => "invoices", "show_in_invoice" => true, "related_to_id" => $invoice_id))->getResult();
             $data['client_info']->custom_fields = $ci->Custom_field_values_model->get_details(array("related_to_type" => "clients", "show_in_invoice" => true, "related_to_id" => $data['invoice_info']->client_id))->getResult();
             return $data;
@@ -202,7 +265,8 @@ if (!function_exists('get_invoice_making_data')) {
 
 if (!function_exists('get_invoice_making_data_delivery_note')) {
 
-    function get_invoice_making_data_delivery_note($invoice_id) {
+    function get_invoice_making_data_delivery_note($invoice_id)
+    {
         $ci = new Security_Controller_Plugin();
         $invoice_info = $ci->Invoices_model->get_details(array("id" => $invoice_id))->getRow();
         if ($invoice_info) {
@@ -214,18 +278,18 @@ if (!function_exists('get_invoice_making_data_delivery_note')) {
             $data['company_info'] = $ci->Company_model->get_one($data['client_info']->company_id);
             $data['users_info'] = $ci->Users_models->get_one($data['company_info']->Director_id);
             $finance_manager_info = $ci->db->table('team_member_job_info')
-            ->select('*') // Select user_id and job_title_en
-            ->where('user_id', $data['company_info']->Director_id)
-            ->get()
-            ->getRow();
-             $project_id = $ci->db->table('projects')
-            ->select('*') // Select user_id and job_title_en
-    ->where('id', $invoice_info->project_id)
-            ->get()
-            ->getRow();
+                ->select('*') // Select user_id and job_title_en
+                ->where('user_id', $data['company_info']->Director_id)
+                ->get()
+                ->getRow();
+            $project_id = $ci->db->table('projects')
+                ->select('*') // Select user_id and job_title_en
+                ->where('id', $invoice_info->project_id)
+                ->get()
+                ->getRow();
 
-        $data['finance_manager_info'] = $finance_manager_info;
-        $data['project']=$project_id;
+            $data['finance_manager_info'] = $finance_manager_info;
+            $data['project'] = $project_id;
             $data['invoice_info']->custom_fields = $ci->Custom_field_values_model->get_details(array("related_to_type" => "invoices", "show_in_invoice" => true, "related_to_id" => $invoice_id))->getResult();
             $data['client_info']->custom_fields = $ci->Custom_field_values_model->get_details(array("related_to_type" => "clients", "show_in_invoice" => true, "related_to_id" => $data['invoice_info']->client_id))->getResult();
             return $data;
@@ -235,7 +299,8 @@ if (!function_exists('get_invoice_making_data_delivery_note')) {
 
 if (!function_exists('prepare_invoice_pdf')) {
 
-    function prepare_invoice_pdf($invoice_data, $mode = "download") {
+    function prepare_invoice_pdf($invoice_data, $mode = "download")
+    {
         $pdf = new Pdf("invoice");
 
         //if setting is desable then don't show header
@@ -246,7 +311,7 @@ if (!function_exists('prepare_invoice_pdf')) {
         $pdf->setPrintFooter(false);
         $pdf->SetCellPadding(1.5);
         $pdf->setImageScale(1.42);
-        $pdf->setInvoiceData($invoice_data); 
+        $pdf->setInvoiceData($invoice_data);
 
         $pdf->AddPage();
 
@@ -292,28 +357,29 @@ if (!function_exists('prepare_invoice_pdf')) {
     }
 }
 if (!function_exists('prepare_invoice_pdf_delivery_note')) {
-    function prepare_invoice_pdf_delivery_note($invoice_data, $mode = "download") {
+    function prepare_invoice_pdf_delivery_note($invoice_data, $mode = "download")
+    {
         $pdf = new Pdf("invoice");
-                $ci = new Security_Controller_Plugin();
+        $ci = new Security_Controller_Plugin();
 
-        
+
         // Force no header/footer margins
         $pdf->setPrintHeader(true);
         $pdf->setPrintFooter(true);
-        
+
         $pdf->SetCellPadding(0.5);
         $pdf->setImageScale(1.42);
-        
+
         // Set all margins to 0
         $pdf->SetMargins(0, 10, 0);
         $pdf->SetHeaderMargin(0);
         $pdf->SetFooterMargin(150);
-        
+
         $pdf->SetAutoPageBreak(true, 15); // Remove automatic page breaks
         $pdf->setInvoiceData($invoice_data); // 
 
         $pdf->AddPage();
-        
+
 
         // Get page width calculations
         $pageWidthInUserUnits = $pdf->getPageWidth();
@@ -321,32 +387,33 @@ if (!function_exists('prepare_invoice_pdf_delivery_note')) {
 
         if ($invoice_data) {
             $invoice_data["mode"] = clean_data($mode);
-            
+
             // Modified delivery note view
             $html = view("aleelo_plugin\Views/invoices/delivery_note_pdf", $invoice_data);
 
             if ($mode != "html") {
                 $html = rebuild_html($html, $pageWidthInPixels);
-                
+
                 // Start writing at the very top of the page
                 $pdf->SetY(0);
                 $pdf->writeHTML($html, true, false, true, false, '');
             }
 
             $invoice_info = get_array_value($invoice_data, "invoice_info");
-$project = $ci->db->table('projects')
-    ->select('*')
-    ->where('id', $invoice_info->project_id)
-    ->get()
-    ->getRow();
+            $project = $ci->db->table('projects')
+                ->select('*')
+                ->where('id', $invoice_info->project_id)
+                ->get()
+                ->getRow();
 
-$company_name = isset($invoice_info->company_name) ? $invoice_info->company_name : "Unknown Company";
+            $company_name = isset($invoice_info->company_name) ? $invoice_info->company_name : "Unknown Company";
 
-if ($project) {
-    $invoice_id = " delivery_note " . $company_name . " " . $project->title;
-} else {
-    $invoice_id =  "delivery_note " . $company_name;
-}            $pdf_file_name =$invoice_id. ".pdf";
+            if ($project) {
+                $invoice_id = " delivery_note " . $company_name . " " . $project->title;
+            } else {
+                $invoice_id =  "delivery_note " . $company_name;
+            }
+            $pdf_file_name = $invoice_id . ".pdf";
 
             if ($mode === "download") {
                 $pdf->Output($pdf_file_name, "D");
@@ -366,7 +433,8 @@ if ($project) {
 }
 if (!function_exists('prepare_invoice_pdf_view')) {
 
-    function prepare_invoice_pdf_view($invoice_data, $mode = "download") {
+    function prepare_invoice_pdf_view($invoice_data, $mode = "download")
+    {
         $pdf = new Pdf("invoice");
 
         //if setting is desable then don't show header
@@ -377,7 +445,7 @@ if (!function_exists('prepare_invoice_pdf_view')) {
         $pdf->setPrintFooter(false);
         $pdf->SetCellPadding(1.5);
         $pdf->setImageScale(1.42);
-                $pdf->setInvoiceData($invoice_data); // 
+        $pdf->setInvoiceData($invoice_data); // 
 
         $pdf->AddPage();
 
@@ -430,14 +498,16 @@ if (!function_exists('prepare_invoice_pdf_view')) {
 
 if (!function_exists("get_logo_url_company")) {
 
-    function get_logo_url_company($department_id) {
+    function get_logo_url_company($department_id)
+    {
         return get_file_from_setting_company($department_id);
     }
 }
 
 if (!function_exists("get_file_from_setting")) {
 
-    function get_file_from_setting_company($department_id = "", $only_file_path_with_slash = false, $file_path = "", $company_id = null) {
+    function get_file_from_setting_company($department_id = "", $only_file_path_with_slash = false, $file_path = "", $company_id = null)
+    {
         if ($department_id) {
             // Load the Company_model
             $Company_model = model('App\Models\Company_model');
@@ -474,7 +544,7 @@ if (!function_exists("get_file_from_setting")) {
         //                 return "/" . ($file_path . $setting_value);
         //             } else {
         //                 return get_file_uri($file_path . $setting_value);
-                        
+
         //             }
         //         }
         //     }
@@ -527,7 +597,8 @@ if (!function_exists("get_file_from_setting")) {
 
 if (!function_exists('demo_get_source_url')) {
 
-    function demo_get_source_url($demo_file = "") {
+    function demo_get_source_url($demo_file = "")
+    {
         if (!$demo_file) {
             return "";
         }
@@ -538,8 +609,6 @@ if (!function_exists('demo_get_source_url')) {
                 return get_source_url_of_file($file, get_demo_setting("demo_file_path"), "thumbnail", false, false, true);
             }
         } catch (\Exception $ex) {
-            
         }
     }
-
 }
