21f4e1a5cc8ff0fe27125f2f1b8ffaa1e9c2b938
azza20435 <zuuzaali12@gmail.com>
2025-07-19 22:01:49 +0300

    estimate pdf job title

--- plugins/aleelo_plugin/Views/estimates/estimate_pdf.php
+++ plugins/aleelo_plugin/Views/estimates/estimate_pdf.php
@@ -416,8 +416,11 @@ $data = array(
 }?>
 
 <div class="signature-block">
+    <?php if($users_info->first_name){?>
     <h5 class="signature-name"><?php echo $users_info->first_name . " " . $users_info->last_name; ?></h5>
+    <?php } if($finance_manager_info->job_title_en){?>
     <h5 class="signature-title"><?php echo $finance_manager_info->job_title_en; ?></h5>
+    <?php } ?>
 </div>
 
 <br/>
