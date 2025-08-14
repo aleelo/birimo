133906f3b5b1d629b8fc54c7392398cad8969e26
azza20435 <zuuzaali12@gmail.com>
2025-08-14 11:50:13 +0300

    expense_deposit_to

--- plugins/Accounting/Models/Accounting_model.php
+++ plugins/Accounting/Models/Accounting_model.php
@@ -9695,8 +9695,8 @@ class Accounting_model extends Crud_model {
                     }
                     $payment_mode_mapping = $this->get_payment_mode_mapping($Expense_payments->payment_method);
                     $node = [];
-                    $node['split'] = $payment_mode_mapping->expense_deposit_to;
-                    $node['account'] = $payment_mode_mapping->expense_payment_account;
+                    $node['split'] = $payment_mode_mapping->expense_payment_account;
+                    $node['account'] = $payment_mode_mapping->expense_deposit_to;
                     $node['date'] = $expense->expense_date;
                     $node['debit'] = $Expense_payments->amount_paid;
                     $node['customer'] = $expense->client_id;
@@ -9710,9 +9710,9 @@ class Accounting_model extends Crud_model {
                     $data_insert[] = $node;
 
                     $node = [];
-                    $node['split'] = $payment_mode_mapping->expense_payment_account;
+                    $node['split'] = $payment_mode_mapping->expense_deposit_to;
+                    $node['account'] = $payment_mode_mapping->expense_payment_account;
                     $node['customer'] = $expense->client_id;
-                    $node['account'] = $payment_mode_mapping->expense_deposit_to;
                     $node['date'] = $expense->expense_date;
                     $node['tax'] = 0;
                     $node['debit'] = 0;
